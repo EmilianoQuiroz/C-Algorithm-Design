db7067aec4049e845be30d4d63b72f56021de248
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-12-25 21:32:11 +0000

    Funcion para pedir carta

--- 05-EjercicioBlackJack/assets/js/juego.js
+++ 05-EjercicioBlackJack/assets/js/juego.js
@@ -10,6 +10,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+//Funcion para crear el mazo
 const crearDeck = () => {
 
     for (let i = 2; i <= 10; i++) {
@@ -23,10 +24,24 @@ const crearDeck = () => {
             deck.push(tipo + especial);
         }
     }
-    console.log(deck);
+
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
 }
 
 crearDeck();
+
+//Funcion para pedir una carta
+const pedirCarta = () => {
+
+    if (deck.length === 0) {
+        throw 'No hay cartas';
+    }
+
+    const carta = deck.pop();
+    console.log(carta)
+    return carta;
+}
+
+pedirCarta();
