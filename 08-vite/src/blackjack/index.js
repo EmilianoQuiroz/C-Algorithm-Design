65fd36674f0b9cfe27167930f50479a7d5ccd7f3
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2024-01-27 20:59:04 +0000

    Modulos por defecto e individuales

--- 08-vite/src/blackjack/index.js
+++ 08-vite/src/blackjack/index.js
@@ -1,5 +1,5 @@
 import _ from 'underscore';
-import { crearDeck } from './usecases/crear-deck'
+import { crearDeck as crearNuevoDeck} from './usecases/crear-deck'
 
 /**
  * 2C = 2 de trebol
@@ -26,7 +26,7 @@ const divCartasComputadora = document.querySelector('#computadora-cartas');
 
 const puntosHTML = document.querySelectorAll('small');
 
-crearDeck();
+deck = crearNuevoDeck(tipos, especiales);
 
 
 //Funcion para pedir una carta
