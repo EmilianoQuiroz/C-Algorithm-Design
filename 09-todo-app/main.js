7660a0e2e31d8c1e3c490a5b9a836ae592473a13
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2024-02-10 20:28:03 +0000

    Fin Todo App

--- 09-todo-app/main.js
+++ 09-todo-app/main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import { App } from './src/todos/app';
-
-import todoStore from './src/stores/tod.store';
+import todoStore from './src/store/todo.store';
 
 todoStore.initStore();
+
 App('#app');
