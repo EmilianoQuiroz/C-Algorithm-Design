ebf240b703597dc166af5f05e75129ed87678636
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2024-02-13 22:20:45 +0000

    Funciones generadoras

--- 10-callbacks-promesas/main.js
+++ 10-callbacks-promesas/main.js
@@ -8,6 +8,7 @@ import { asyncComponent } from './src/concepts/05-async'
 import { asyncAwaitComponent } from './src/concepts/06-async-await'
 import { asyncAwait2Component } from './src/concepts/07-async-await'
 import { forAwaitComponent } from './src/concepts/08-for-await'
+import { generatorFunctionsComponent } from './src/concepts/09-generators'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -24,4 +25,4 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 const element = document.querySelector('.card');
-forAwaitComponent(element);
+generatorFunctionsComponent(element);
