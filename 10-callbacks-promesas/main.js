1fae7ca8f8bf82aa66474a3c32f42739e216884e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2024-02-13 21:49:38 +0000

    async - await

--- 10-callbacks-promesas/main.js
+++ 10-callbacks-promesas/main.js
@@ -6,6 +6,7 @@ import { promiseComponent } from './src/concepts/03-promises'
 import { promiseRaceComponent } from './src/concepts/04-promise-race'
 import { asyncComponent } from './src/concepts/05-async'
 import { asyncAwaitComponent } from './src/concepts/06-async-await'
+import { asyncAwait2Component } from './src/concepts/07-async-await'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -22,4 +23,4 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 const element = document.querySelector('.card');
-asyncAwaitComponent(element);
+asyncAwait2Component(element);
