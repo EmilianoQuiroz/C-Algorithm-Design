74ec6f0559650e295a4ec25ce9bab59c915a9222
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2024-02-18 19:43:11 +0000

    Peticiones HTTP

--- 11-peticiones-http/src/breakingbad/breaking-bad-app.js
+++ 11-peticiones-http/src/breakingbad/breaking-bad-app.js
@@ -1,11 +1,45 @@
 /**
+ * @returns {Objet} quote information
+ */
+const fetchQuote = async() => {
+
+    const res = await fetch(`https://api.breakingbadquotes.xyz/v1/quotes`);
+    const data = await res.json();
+
+    return data[0];
+}
+
+
+/**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = (element) => {
+export const BreakingbadApp = async (element) => {
 
-    console.log(element)
+    console.log('nada');
 
     document.querySelector('#app-title').innerHTML = 'Breakingbad App'
-    element.innerHTML = 'Loading...'
+    element.innerHTML = 'Loading...';
+
+    //await fetchQuote();
+
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+    }
+
+    // Listener del boton
+    nextQuoteButton.addEventListener('click', async() =>{
+        element.innerHTML = 'Loading...';
+        const quote = await fetchQuote();
+        renderQuote(quote);
+    })
+    fetchQuote()
+        .then(renderQuote);
 }
